0d6813a041d7e87daaec9da7ad2b72f4a0a4b2f3
Ini Akinleye <inioluwaakinleye@gmail.com>
2019-04-11 21:19:49 +0100

    add filterText function

--- app.js
+++ app.js
@@ -56,8 +56,20 @@ function clearTasks() {
   }
 }
 
-function filterTasks() {
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
 
+  document.querySelectorAll('.collection-item').forEach
+  (function (task) {
+    const item = task.firstChild.textContent;
+
+    if (item.toLowerCase().indexOf(text) !== -1) {
+      task.style.display = 'block';
+    }
+    else {
+      task.style.display = 'none';
+    }
+  });
 }
 
 // Load all event listeners
